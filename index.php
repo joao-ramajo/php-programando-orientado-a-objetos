1d2e7578bb93947dddfde801ee1b61555b58bf6c
LacamJC <joaoramajo744@gmail.com>
2025-03-16 00:24:27 -0300

    add files

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>First attempt with php poo book</title>
+    </head>
+    <body>
+        <?php
+        include "./mysql_insere.php";
+        include "./mysql_lista.php";
+        
+        
+        ?>
+        
+        
+    </body>
+</html>
+
+        
